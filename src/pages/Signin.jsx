8f25f0c0bffd0f2f69ceaa523c7930f473fec6a9
Mochammad Syahrul Kurniawan <mochammadsk99@gmail.com>
2024-12-24 19:20:54 +0800

    add signin pages

---
+++ src/pages/Signin.jsx
@@ -0,0 +1,35 @@
+import Navbar from '../elements/Navbar';
+import Form from '../components/Form';
+import Button from '../components/Button';
+
+const Signin = () => {
+  const handleSignin = (e) => {
+    e.preventDefault();
+  };
+
+  return (
+    <>
+      <Navbar />
+      <Form onSubmit={handleSignin}>
+        <input
+          type='email'
+          placeholder='Email'
+          className='border-b-2 outline-none px-3 py-2 bg-white'
+        />
+        <input
+          type='password'
+          placeholder='Password'
+          className='border-b-2 outline-none px-3 py-2 bg-white'
+        />
+        <Button type='button' variant={'primary'} className='w-56'>
+          Login
+        </Button>
+        <p>
+          Belum punya akun? <a href='/signup'>Daftar</a>
+        </p>
+      </Form>
+    </>
+  );
+};
+
+export default Signin;
