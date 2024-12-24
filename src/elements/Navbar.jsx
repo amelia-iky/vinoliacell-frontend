5ff69f6667bf1c3e386697212b2286e9e75e38b5
Mochammad Syahrul Kurniawan <mochammadsk99@gmail.com>
2024-12-24 19:19:13 +0800

    update dropdown menu auth

--- src/elements/Navbar.jsx
+++ src/elements/Navbar.jsx
@@ -1,39 +1,68 @@
+import { useState } from 'react';
 import { FaRegCircleUser } from 'react-icons/fa6';
 
 const Navbar = () => {
+  const [showMenu, setShowMenu] = useState(false);
+
+  // Toggle menu visibility
+  const toggleMenu = () => {
+    setShowMenu((prev) => !prev);
+  };
+
   return (
-    <div className='flex flex-col bg-white'>
+    <div className='flex flex-col bg-white shadow-lg'>
+      {/* Header */}
       <div className='flex flex-row justify-between items-center border-b-2 border-slate-500 py-5'>
         <div className='flex flex-row gap-4 px-20'>
           <h1 className='text-3xl font-bold'>Vinolia Cell</h1>
           <img className='w-28' src='/icons/logo.png' alt='logo' />
         </div>
-        <div className='px-20'>
-          <FaRegCircleUser className='text-5xl' />
+
+        {/* User Icon and Dropdown */}
+        <div className='relative px-20'>
+          <FaRegCircleUser
+            className='text-5xl cursor-pointer'
+            onClick={toggleMenu} // Toggle menu on click
+          />
+
+          {/* Dropdown Menu */}
+          {showMenu && (
+            <div className='absolute right-3 mt-2 w-48 bg-white border border-gray-300 rounded-md shadow-lg'>
+              <ul className='flex flex-col'>
+                <li className='px-4 py-2 hover:bg-primary'>
+                  <a href='/signin'>Login</a>
+                </li>
+                <li className='px-4 py-2 hover:bg-primary'>
+                  <a href='#register'>Register</a>
+                </li>
+              </ul>
+            </div>
+          )}
         </div>
       </div>
 
+      {/* Navigation */}
       <div className='flex flex-row justify-center items-center gap-10 h-14 py-2 border-b-2 border-slate-500'>
         <a
-          className='hover:border hover:boder-2 hover:rounded-md hover:bg-slate-50 p-2'
-          href='#'
+          className='hover:border-2 hover:rounded-md hover:bg-slate-50 p-2'
+          href='/'
         >
           Halaman Saya
         </a>
         <a
-          className='hover:border hover:boder-2 hover:rounded-md hover:bg-slate-50 p-2'
+          className='hover:border-2 hover:rounded-md hover:bg-slate-50 p-2'
           href='#'
         >
           Layanan Perbaikan
         </a>
         <a
-          className='hover:border hover:boder-2 hover:rounded-md hover:bg-slate-50 p-2'
+          className='hover:border-2 hover:rounded-md hover:bg-slate-50 p-2'
           href='#'
         >
           Riwayat
         </a>
         <a
-          className='hover:border hover:boder-2 hover:rounded-md hover:bg-slate-50 p-2'
+          className='hover:border-2 hover:rounded-md hover:bg-slate-50 p-2'
           href='#'
         >
           Tentang Kami
