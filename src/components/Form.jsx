2dbf3ee01a2838653bf316b60ebcea6a8e6c377b
Mochammad Syahrul Kurniawan <mochammadsk99@gmail.com>
2024-12-24 19:55:48 +0800

    add form components

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,20 +1,18 @@
 import PropTypes from 'prop-types';
 
-const Form = ({ children, onSubmit }) => {
+const Form = ({ children, onSubmit, className = '' }) => {
   return (
-    <div className='relative flex justify-center items-center h-full mt-16'>
-      <div className='relative w-full h-96 max-w-md bg-shadowform'>
-        <div className='absolute w-full h-96 right-3 bottom-7 bg-white'>
-          <form
-            className='flex flex-col justify-center items-center py-7 px-10 gap-6 bg-white'
-            onSubmit={onSubmit}
-          >
-            <h1 className='text-xl font-bold text-center'>
-              Satu akun. Apa pun perangkatnya. Khusus untuk Anda
-            </h1>
-            <p className='text-sm'>Masuk untuk mulai</p>
-            {children}
-          </form>
+    <div className='flex justify-center items-center'>
+      <div className='relative flex justify-center items-center w-4/12 h-full mt-16'>
+        <div className='relative w-full h-96 max-w-md bg-shadowform'>
+          <div className='absolute w-full h-96 right-4 bottom-8 bg-white'>
+            <form
+              className={`flex flex-col justify-center items-center px-10 gap-6 bg-white ${className}`}
+              onSubmit={onSubmit}
+            >
+              {children}
+            </form>
+          </div>
         </div>
       </div>
     </div>
