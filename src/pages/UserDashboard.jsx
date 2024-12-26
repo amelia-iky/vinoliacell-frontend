d0b44367e29becab75d2999abbb07478ae8d4fe3
Mochammad Syahrul Kurniawan <mochammadsk99@gmail.com>
2024-12-26 19:05:46 +0800

    new pages for user dashboard

---
+++ src/pages/UserDashboard.jsx
@@ -0,0 +1,13 @@
+import Card from '../elements/UserDashboard/Card';
+import Navbar from '../elements/Navbar';
+
+const UserDashboard = () => {
+  return (
+    <>
+      <Navbar />
+      <Card />
+    </>
+  );
+};
+
+export default UserDashboard;
