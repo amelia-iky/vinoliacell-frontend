c740c4dda8c2a1ac15a45e9939f909f16be5633d
Mochammad Syahrul Kurniawan <mochammadsk99@gmail.com>
2024-12-26 19:04:49 +0800

    call user dashboard routes

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import LandingPages from './pages/LandingPages';
 import Signin from './pages/Signin';
 import Signup from './pages/Signup';
+import UserDashboard from './pages/UserDashboard';
 
 const App = () => {
   return (
@@ -12,9 +13,15 @@ const App = () => {
       }}
     >
       <Routes>
+        {/* Landing Pages */}
         <Route path='/' element={<LandingPages />} />
+
+        {/* Authentication */}
         <Route path='/signin' element={<Signin />} />
         <Route path='/signup' element={<Signup />} />
+
+        {/* User Dashboard */}
+        <Route path='/dashboard' element={<UserDashboard />} />
       </Routes>
     </BrowserRouter>
   );
