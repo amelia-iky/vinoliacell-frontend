f98a139086098f44ea9425b229ff6b76575beb9c
Mochammad Syahrul Kurniawan <mochammadsk99@gmail.com>
2024-12-27 20:26:36 +0800

    fix use Navbar

--- src/pages/UserDashboard.jsx
+++ src/pages/UserDashboard.jsx
@@ -1,5 +1,5 @@
-import Card from '../elements/UserDashboard/Card';
 import Navbar from '../elements/Navbar';
+import Card from '../elements/UserDashboard/Card';
 
 const UserDashboard = () => {
   return (
