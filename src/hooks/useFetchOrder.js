8362b0f9ea930088e62f36295ca04ede9ae30602
Mochammad Syahrul Kurniawan <mochammadsk99@gmail.com>
2025-01-02 21:57:33 +0800

    create new hooks for fetch order data

---
+++ src/hooks/useFetchOrder.js
@@ -0,0 +1,36 @@
+import { useState, useEffect } from 'react';
+import axios from 'axios';
+
+export const useFetchOrder = () => {
+  const [data, setData] = useState(null);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    const fetchOrder = async () => {
+      setLoading(true);
+      setError(null);
+
+      // Get token
+      const token = sessionStorage.getItem('token');
+
+      try {
+        const response = await axios.get('http://localhost:8000/api/orders', {
+          headers: {
+            Authorization: `Bearer ${token}`,
+          },
+        });
+
+        setData(response.data.data);
+      } catch (error) {
+        setError(error);
+      } finally {
+        setLoading(false);
+      }
+    };
+
+    fetchOrder();
+  }, []);
+
+  return { data, loading, error };
+};
