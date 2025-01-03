042c9754dc3e62c96accac8dcce9ecda2944edef
Mochammad Syahrul Kurniawan <mochammadsk99@gmail.com>
2025-01-03 17:17:15 +0800

    refactor Button component to ButtonIcon and add support for rendering an icon

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,13 +1,14 @@
 import PropTypes from 'prop-types';
 
-const Button = ({ type, onClick, variant, children }) => {
-  const baseClass = 'py-2 px-4 w-2/3 rounded-3xl font-semibold';
+const ButtonIcon = ({ type, onClick, variant, children, icon: Icon }) => {
+  const baseClass =
+    'flex items-center gap-2 py-2 px-4 rounded-3xl font-semibold';
 
   const variantClass = {
-    primary: 'bg-blue-400 hover:bg-blue-500 text-white',
+    primary: 'bg-blue-500 hover:bg-blue-600 text-white',
     secondary:
       'bg-transparent border border-primary-dark text-[#515151] text-sm',
-    deleted: 'bg-red-800 text-white hover:bg-red-700',
+    deleted: 'bg-red-500 text-white hover:bg-red-600',
     disabled: 'bg-gray-300 text-gray-500 cursor-not-allowed',
   };
 
@@ -22,23 +23,26 @@ const Button = ({ type, onClick, variant, children }) => {
       className={`${baseClass} ${variantClass[variant] || ''}`}
       disabled={variant === 'disabled'}
     >
+      {Icon && <Icon className='text-lg' />} {/* Render the icon if provided */}
       {children}
     </button>
   );
 };
 
 // Validasi props menggunakan PropTypes
-Button.propTypes = {
+ButtonIcon.propTypes = {
   type: PropTypes.string,
   onClick: PropTypes.func,
   variant: PropTypes.oneOf(['primary', 'secondary', 'deleted', 'disabled']),
   children: PropTypes.node.isRequired,
+  icon: PropTypes.elementType, // Validates React components for icons
 };
 
 // Nilai default untuk props
-Button.defaultProps = {
+ButtonIcon.defaultProps = {
   type: 'button',
   variant: 'primary',
+  icon: null, // Default is no icon
 };
 
-export default Button;
+export default ButtonIcon;
