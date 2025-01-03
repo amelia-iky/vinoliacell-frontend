62247856d7297a89e451d5e764e24f3ea1c0bfd9
Mochammad Syahrul Kurniawan <mochammadsk99@gmail.com>
2025-01-03 17:17:25 +0800

    replace user name display with TitlePages component in AdminDashboard

--- src/pages/AdminDasboard.jsx
+++ src/pages/AdminDasboard.jsx
@@ -1,21 +1,14 @@
 import Navbar from '../elements/Navbar';
+import TitlePages from '../elements/AdminPages/TitlePages';
 import Card from '../elements/AdminPages/Card';
 import OrderTable from '../elements/AdminPages/OrderTable';
-import { getUserName } from '../utils/Auth';
 
 const AdminDashboard = () => {
-  const userName = getUserName();
-
   return (
     <>
       <Navbar />
       <div className='mx-32'>
-        <div className='flex flex-col gap-4 py-10 text-2xl font-semibold'>
-          <h1>
-            Selamat Datang <span className='font-bold'>{userName}</span>
-          </h1>
-          <h1>DASHBOARD</h1>
-        </div>
+        <TitlePages />
         <Card />
         <OrderTable />
       </div>
