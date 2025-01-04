d9b40bdb4aa5a5c060a89af6b1b93609df86add0
Mochammad Syahrul Kurniawan <mochammadsk99@gmail.com>
2025-01-04 16:45:26 +0800

    fix: update Card component to display user count and adjust order status counts

--- src/elements/AdminPages/Card.jsx
+++ src/elements/AdminPages/Card.jsx
@@ -2,9 +2,11 @@ import { SlBasket } from 'react-icons/sl';
 import { SlBasketLoaded } from 'react-icons/sl';
 import { GrGroup } from 'react-icons/gr';
 import { useFetchOrder } from '../../hooks/useFetchOrder';
+import { useFetchUserData } from '../../hooks/useFetchUserData';
 
 const Card = () => {
   const { data } = useFetchOrder();
+  const { userData } = useFetchUserData();
 
   const countByStatus = (status) =>
     data ? data.filter((item) => item.status === status).length : 0;
@@ -14,7 +16,7 @@ const Card = () => {
       <div className='flex flex-col bg-white justify-center shadow-md rounded-lg px-5 py-3 w-1/2 gap-3'>
         <h2 className='text-xl font-bold'>Jumlah Pelanggan</h2>
         <div className='flex flex-row items-center justify-between'>
-          <p className='text-2xl'>2</p>
+          <p className='text-2xl'>{userData ? userData?.length : 0}</p>
           <GrGroup className='text-4xl' />
         </div>
       </div>
@@ -22,7 +24,10 @@ const Card = () => {
       <div className='flex flex-col bg-white justify-center shadow-md rounded-lg px-5 py-3 w-1/2 gap-3'>
         <h2 className='text-xl font-bold'>Jumlah Pesanan Aktif</h2>
         <div className='flex flex-row items-center justify-between'>
-          <p className='text-2xl'>{countByStatus('Belum Selesai')}</p>
+          <p className='text-2xl'>
+            {countByStatus('Pesanan Diterima') +
+              countByStatus('Proses Perbaikan')}
+          </p>
           <SlBasketLoaded className='text-4xl' />
         </div>
       </div>
@@ -30,7 +35,7 @@ const Card = () => {
       <div className='flex flex-col bg-white justify-center shadow-md rounded-lg px-5 py-3 w-1/2 gap-3'>
         <h2 className='text-xl font-bold'>Jumlah Pesanan Selesai</h2>
         <div className='flex flex-row items-center justify-between'>
-          <p className='text-2xl'>{countByStatus('Selesai')}</p>
+          <p className='text-2xl'>{countByStatus('Perbaikan Selesai')}</p>
           <SlBasket className='text-4xl' />
         </div>
       </div>
